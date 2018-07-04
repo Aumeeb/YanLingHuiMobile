20ecff8ea549520c7182283650676149eaa55bc1
lixingchen <noberkli@gmail.com>
2018-07-05 01:50:56 +0800

    代码缩减

--- docs/YanLingHui/src/ts/utils/decorators.ts
+++ docs/YanLingHui/src/ts/utils/decorators.ts
@@ -1,17 +1,22 @@
-export const Header = function (title: string) {
+interface Inventory {
+          styleUrls: string[];
+          selector?: string
+}
+
+export const Header = (title: string) => {
           return target => { target.title = title }
-  }
-  export const Component = function (inventory: { styleUrls: string[] }) {
+}
+export const Component = (inventory: Inventory) => {
           return target => {
-                  let head = document.getElementsByTagName('head')[0];
-  
-  
-                  inventory.styleUrls.forEach(cssUrl => {
-                          let link = document.createElement('link');
-                          link.type = 'text/css';
-                          link.rel = 'stylesheet';
-                          link.href = cssUrl;
-                          head.appendChild(link);
-                  })
+                    let head = document.getElementsByTagName('head')[0];
+
+
+                    inventory.styleUrls.forEach(cssUrl => {
+                              let link = document.createElement('link');
+                              link.type = 'text/css';
+                              link.rel = 'stylesheet';
+                              link.href = cssUrl;
+                              head.appendChild(link);
+                    })
           }
-  }
+}
