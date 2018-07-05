5597d2c9895a6bde68737ddb334f653b7f226286
noberk <noberkli@gmail.com>
2018-07-05 17:13:17 +0800

    老专家页面

--- docs/YanLingHui/src/ts/expert.ts
+++ docs/YanLingHui/src/ts/expert.ts
@@ -3,12 +3,12 @@ import * as $ from "jquery";
 import { Template } from "./template";
 import { Component, Header } from "./utils/decorators";
 
-@Component({ styleUrls: ["./css/expert.css"] })
+@Component({ styleUrls: ["./css/expert.css","./css/index.css"] })
 @Header("名医汇")
 class Expert extends Template {
     constructor() {
         super();
-        this.render(this.headerNoUrl(Expert.title));
+        this.render(this.headerNoUrl(Expert.title),this.homeTab());
         this.changeCategoryFontColor();
     }
     private changeCategoryFontColor() {
