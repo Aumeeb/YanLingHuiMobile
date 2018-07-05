44f51d9cb1f04af467a5f614250df1c9b538e602
chanbaihai <chanbaihai@outlook.com>
2018-07-05 08:41:55 +0800

    解决冲突

---
+++ docs/YanLingHui/src/ts/personal_center.ts
@@ -0,0 +1,9 @@
+import {Template,ROUTER} from "./template"
+class Tem extends Template{
+    constructor(){
+        super();
+        this.render(this.headerWithUrl({title:"个人中心",url:ROUTER.index}));
+        this.render(null,this.Hometab())
+    }
+};
+new Tem()
