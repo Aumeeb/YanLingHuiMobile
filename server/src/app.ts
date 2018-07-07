99ccd00ef58ebc6cb540b139213535f4478f0301
noberk <noberkli@gmail.com>
2018-07-07 14:27:16 +0800

    添加一键启动

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,11 +3,11 @@ import { AddressInfo } from "net";
 
 var app = express();
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(3000, () => {
   var host = (server.address() as AddressInfo).address;
   var port = (server.address() as AddressInfo).port;
 
